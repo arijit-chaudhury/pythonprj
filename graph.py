0e068f8e75dbb3fe528720eb2082fde9dddeceea
ARIJIT CHAUDHURY <34763856+arijit-chaudhury@users.noreply.github.com>
2024-12-12 18:59:25 +0000

    Scatter graph plotted

--- graph.py
+++ graph.py
@@ -7,7 +7,7 @@ df = pd.read_csv('mpg.csv')
 df.boxplot(ax=ax)
 ax.set_xticks(range(10))
 ax.set_xticklabels(range(10))
-plt.show()
+#plt.show()
 # first count number of rows and columns
 # find number of columns from 2nd element of the array
 n_box_plots = df.select_dtypes(include='number').shape[1]
@@ -16,6 +16,23 @@ print(n_box_plots)
 # get box plot from 'cylinders' & 'model_year'
 ax = df[['cylinders','model_year']].plot.box()
 all_labels = ax.get_yticks()
-smallest_label = min(all_labels)
+smallest_label = all_labels[1]
 largest_label = max(all_labels)
 print(all_labels,smallest_label,largest_label)
+
+# plot hist diagram
+df.plot.hist()
+#plt.show()
+ax = df[['acceleration']].plot.hist()
+#plt.show()
+xlabels = ax.patches
+for patch in xlabels:
+    print(patch.get_height())
+
+approx_peak_height = max(patch.get_height() for patch in ax.patches)
+print(approx_peak_height)
+
+# Scatter graph
+ax = df[['acceleration','mpg']].plot.scatter(x = 'acceleration', y = 'mpg')
+ylabels = ax.get_yticks()
+largest_mpg_label = ylabels[-2]
