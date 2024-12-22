b3f7c571463c0332f1e09dd65ca791157ad31d77
ARIJIT CHAUDHURY <34763856+arijit-chaudhury@users.noreply.github.com>
2024-12-22 22:25:33 +0000

    DB connection

---
+++ database_connect.py
@@ -0,0 +1,9 @@
+import sqlalchemy
+from sqlalchemy import create_engine, select, MetaData, Table, and_
+
+engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/postgres')
+conn = engine.connect()
+metadata = sqlalchemy.MetaData()
+assetdistribution = sqlalchemy.Table('assetdistribution', metadata, autoload_with=engine)
+
+print(assetdistribution.columns.keys())
