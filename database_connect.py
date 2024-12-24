5b9508c873d64644bc87705030438e37f8f7c060
ARIJIT CHAUDHURY <34763856+arijit-chaudhury@users.noreply.github.com>
2024-12-24 18:19:24 +0000

    restructuring database operations

--- database_connect.py
+++ database_connect.py
@@ -3,12 +3,3 @@ from sqlalchemy import create_engine, select, MetaData, Table, and_
 
 engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/postgres')
 conn = engine.connect()
-metadata = sqlalchemy.MetaData()
-assetdistribution = sqlalchemy.Table('assetdistribution', metadata, autoload_with=engine)
-
-print(assetdistribution.columns.keys())
-query = sqlalchemy.select([assetdistribution])
-resultProxy = conn.execute(query)
-result = resultProxy.fetchall()
-for r in result:
-    print(r)
