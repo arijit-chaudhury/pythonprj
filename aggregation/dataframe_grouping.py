03c0f7a975e7c5780cc18ba0ad5117b29b0b6684
ARIJIT CHAUDHURY <34763856+arijit-chaudhury@users.noreply.github.com>
2024-12-29 15:34:22 +0000

    Dataframe grouping

--- aggregation/dataframe_grouping.py
+++ aggregation/dataframe_grouping.py
@@ -1,18 +1,27 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('../customers-100.csv')
 print(df.head())
 
 df['Date'] = pd.to_datetime(df['Subscription Date'], errors='coerce')
-print(df['Date'])
+print(df.head())
 
-df2 = df[['Customer Id', 'Country', 'Subscription Date']]
+df2 = df[['Customer Id', 'Date']]
 print(df2.head())
 
 # Group by subscription date
-noOfOccurrence = df2.groupby(['Subscription Date']).size().reset_index(name='noOfOccurrence')
+noOfOccurrence = df2.groupby(['Date']).size().reset_index(name='noOfOccurrence')
 print(noOfOccurrence)
 
-print(df2['Subscription Date'].min())
-print(df2['Subscription Date'].max())
+print(df2['Date'].min())
+print(df2['Date'].max())
+
+noOfOccurrence.plot.scatter(x='Date', y='noOfOccurrence')
+plt.show()
+
+
+
+
+
 
