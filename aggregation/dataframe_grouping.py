3abbaaeb322305e6843e87cf4e47b666cd1d8ed9
ARIJIT CHAUDHURY <34763856+arijit-chaudhury@users.noreply.github.com>
2024-12-29 20:46:11 +0000

    Loop in range and find out count

--- aggregation/dataframe_grouping.py
+++ aggregation/dataframe_grouping.py
@@ -5,14 +5,14 @@ import numpy as np
 
 df = pd.read_csv('../customers-100.csv')
 df['Date'] = pd.to_datetime(df['Subscription Date'], errors='coerce')
-print(df.head())
+# print(df.head())
 
 df2 = df[['Customer Id', 'Date']]
-print(df2.head())
+# print(df2.head())
 
 # Group by subscription date
 noOfOccurrence = df2.groupby(['Date']).size().reset_index(name='noOfOccurrence')
-print(noOfOccurrence)
+# print(noOfOccurrence)
 
 # find min and max values
 min_date = df2['Date'].min()
@@ -20,18 +20,26 @@ max_date = df2['Date'].max()
 
 # Try date range from minimum and maximum value
 # per1 = pd.date_range(start=min_date, end=max_date, freq='ME')
-#for val in per1:
+# for val in per1:
 #    print(val)
 dt = timedelta(days=30)
 dates = np.arange(min_date, max_date, dt).astype(datetime)
-print(dates)
+# print(dates)
 
 n = pd.period_range(start=min_date, end=max_date, freq='M')
-print(n)
+# print(n)
+
+while min_date <= max_date:
+    print(min_date)
+    print(min_date+dt)
+    df4 = df2[(df['Date'] > min_date) & (df2['Date'] <= min_date+dt)]
+    min_date = min_date+dt
+    print(df4['Customer Id'].count())
+    print('\n')
 
 filterData = (df2['Date'] > min_date) & (df2['Date'] <= max_date)
 df3 = df2.loc[filterData]
-print(df3)
+# print(df3)
 
 noOfOccurrence.plot.scatter(x='Date', y='noOfOccurrence')
-#plt.show()
+# plt.show()
