2a467de6e62ff1badd6841222dcdf6521f6ccfd1
ARIJIT CHAUDHURY <34763856+arijit-chaudhury@users.noreply.github.com>
2025-01-21 21:31:18 +0000

    Udemy learning - multiple axes in a single figure

---
+++ modelling/multiple_axis_graph.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv("mpg.csv")
+fig = plt.figure(figsize=(20, 10))
+
+axes1 = fig.add_axes([0, 0, 1, 1])
+axes2 = axes1.twinx()
+
+axes1.bar('mpg', 'weight', data=df, color='#CE7BB0')
+axes2.plot('mpg', 'horsepower', data=df, color='#6867AC')
+
+plt.legend()
+
+plt.show()
